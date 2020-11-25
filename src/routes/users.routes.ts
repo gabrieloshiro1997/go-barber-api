167bfde1362333b3f59dc990e802c408f42db621
Gabriel Oshiro <gabrieloshiro1997@gmail.com>
2020-11-24 21:08:17 -0300

    feature: add upload image

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,8 +1,14 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarSevice from '../services/UpdateUserAvatarService';
+
+import ensureAuthenticaded from '../midleware/ensureAuthenticated';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
     try {
@@ -29,5 +35,34 @@ usersRouter.post('/', async (request, response) => {
         return response.status(400).json({ message: error.message });
     }
 });
+
+usersRouter.patch(
+    '/avatar',
+    ensureAuthenticaded,
+    upload.single('avatar'),
+    async (request, response) => {
+        try {
+            const updateUserAvatar = new UpdateUserAvatarSevice();
+
+            const user = await updateUserAvatar.execute({
+                user_id: request.user.id,
+                avatarFileName: request.file.filename,
+            });
+
+            const userWithoutPassword = {
+                id: user.id,
+                name: user.name,
+                email: user.email,
+                avatar: user.avatar,
+                created_ad: user.created_at,
+                updated_at: user.updated_at,
+            };
+
+            return response.json(userWithoutPassword);
+        } catch (error) {
+            return response.status(400).json({ error: error.message });
+        }
+    },
+);
 
 export default usersRouter;
